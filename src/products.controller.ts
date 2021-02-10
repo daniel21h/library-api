0618ad8d8b3ca20fb04e0d2ad4bc78e03e360323
daniel21h <hesseldaniel21@gmail.com>
2021-02-10 20:29:43 -0300

    feat: learning about models on nestjs

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,34 +1,41 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Product } from "./product.model";
 
 @Controller('products')
 export class ProductsController {
+  products: Product[] = [
+    new Product('LIV0001', 'TDD e DDD na prática', 29.90),
+    new Product('LIV0002', 'Dominando a DOM', 19.90),
+    new Product('LIV0003', 'NestJS - Escale sem limites', 49.90),
+  ];
 
   @Get()
-  getAll(): string {
-    return 'List all products.'
+  getAll(): Product[] {
+    return this.products;
   }
 
   @Get(':id')
-  getOne(@Param() params): string {
-    return `Returns product data ${params.id}.`
+  getOne(@Param() params): Product {
+    return this.products[params.id];
   }
 
   @Post()
-  create(@Body() product): string {
-    console.log(product)
+  create(@Body() product: Product): string {
+    product.id = 100;
+    this.products.push(product);
 
-    return 'Created product.'
+    return 'Created product.';
   }
 
   @Put()
-  update(@Body() product): string {
-    console.log(product)
+  update(@Body() product: Product): Product {
 
-    return 'Updated product.'
+    return product
   }
 
   @Delete(':id')
-  delete(@Param() params): string {
-    return `Deleted product ${params.id}.`
+  delete(@Param() params): Product {
+
+    return this.products.pop()
   }
 }
